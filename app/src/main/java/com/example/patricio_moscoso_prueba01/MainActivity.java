e8d728af2a49a75ed774108213ffb4b605c2628b
Patricio Moscoso <patrixiolel@gmail.com>
2023-05-19 16:29:54 -0500

    Patricio Moscoso: 16:28

---
+++ app/src/main/java/com/example/patricio_moscoso_prueba01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.patricio_moscoso_prueba01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
