6b00f21fe4edbbe1d912b592195444151910ed28
Patricio Moscoso <patrixiolel@gmail.com>
2023-05-19 17:02:35 -0500

    Patricio Moscoso: 17:02

--- app/src/main/java/com/example/patricio_moscoso_prueba01/PrimeroActivityMoscoso.java
+++ app/src/main/java/com/example/patricio_moscoso_prueba01/PrimeroActivityMoscoso.java
@@ -3,6 +3,7 @@ package com.example.patricio_moscoso_prueba01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class PrimeroActivityMoscoso extends AppCompatActivity {
@@ -13,12 +14,21 @@ public class PrimeroActivityMoscoso extends AppCompatActivity {
     private TextView numeroUno;
     private TextView numeroDos;
 
-    private TextView resultadoUno;
-    private TextView resultadoDos;
+    private Button siguienteButton;
+    private Button mostrarResultadosButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.primeromoscoso_activity);
+
+        textoNombre = findViewById(R.id.nombretextView);
+        textoNombre = findViewById(R.id.nombretextView);
+        textoNombre = findViewById(R.id.nombretextView);
+        textoNombre = findViewById(R.id.nombretextView);
+        textoNombre = findViewById(R.id.nombretextView);
+        textoNombre = findViewById(R.id.nombretextView);
+        textoNombre = findViewById(R.id.nombretextView);
+
 
     }
 }
